7ffe581d7652cbc980f95a97bd5843a76d7065ac
Georg Mastritsch <georg.mastritsch@gmail.com>
2015-08-13 09:59:35 +0200

    added german language strings

---
+++ lang/de/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * DokuWiki plugin for Piwik2
+ *
+ * Field descriptions for plugin configuration in plugin manager interface
+ *
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl.html)
+ * @author Marcel Lange <info@aasgard.de>
+ */
+$lang['track_admin_user'] = 'Admin-Aktionen verfolgen';
+$lang['track_user'] = 'Benutzerverhalten verfolgen ';
+$lang['js_tracking_code'] = 'Tracking-Code (aus dem Piwik-Backend generiert) ';
+$lang['img_tracking_code'] = 'Tracking-Code für image tracking';
+$lang['use_img_tracking'] = 'benutze image tracking';
